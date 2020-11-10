0263906e14b7913ec09f5edbf45c7bc84fababe5
Denis Feliciano <denisf.salles@gmail.com>
2020-11-10 00:37:46 -0300

    Add Pragraph

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home(){
     <body>
       <h1>Ideia revolucionária e bilionária!!!</h1>
       <Incrementer />
+      <p>Veniam aliqua cillum reprehenderit esse irure aliqua ipsum excepteur amet sit elit. Do esse sunt officia mollit ea. Culpa nisi proident et consectetur ex fugiat adipisicing anim pariatur.</p>
     </body>
   )
 }
@@ -14,7 +15,7 @@ function Incrementer() {
  
   function increment() {
     setCount(count + 1);
-  }
+  } 
   return (
     <div>
       <h1>Count: {count} </h1>
